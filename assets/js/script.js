a6b0a768ab4158a95c1ebd17c8b788879fd039c4
Todd Malone <toddomalone@gmail.com>
2021-01-18 21:11:57 -0600

    save buttons update only their associated timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,18 +13,25 @@ function addTimeBlock(hour){
     var timeRow = $("<div class='row'>");
     var bufferLeft = $("<div class='col-md-1'>");
     var hourBlock = $("<div class='col-md-1 border-top border-bottom border-secondary border-right'>");
-    var textBlock = $("<div class='col-md-8 bg-success border-top border-bottom border-white'>");
-    var saveBlock = $("<div class='col-md-1 bg-primary border-primary rounded-right'><button class='btn btn-primary'>Save</button></div>")
+    var textBlock = $("<input type='text' data-hour-id="+hour+" class='col-md-8 bg-success border-top border-bottom border-white event-text'>");
+    var saveBlock = $("<div class='col-md-1 bg-primary border-primary rounded-right'><button data-button-hour="+hour+" class='btn btn-primary save-button'>Save</button></div>")
     var bufferRight = $("<div class='col-md-1'>");
 
     hourBlock.text(hour);
-    textBlock.text("notes");
+    textBlock.attr("value","notes");
 
     timeRow.append(bufferLeft,hourBlock,textBlock,saveBlock,bufferRight);
     $("#timeblock-container").append(timeRow);
 }
 
+
+
+
 for(var i = dayStart; i<dayEnd;i++){
     addTimeBlock(i);
 }
 
+$(".save-button").on("click", function(){
+    $("[data-hour-id="+$(this).data("button-hour")+"]").attr("value",$(this).data("button-hour")+" clicked!");
+    //$(".event-text").attr("value",$(this).data("button-hour")+" clicked!");
+})
