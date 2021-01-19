85df8c789ec7958237b3d48005b339c6b31988fd
Todd Malone <toddomalone@gmail.com>
2021-01-18 23:17:55 -0600

    added current hour tracker with highlighting; added formatting for hour markers

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,36 @@
 <div class="col-md-1"></div>
 </div> */}
 
-const dayStart = 5
-const dayEnd = 10
+
+//dayStart and dayEnd are in 24-hour format
+const dayStart = 8
+const dayEnd = 17
+var currentHour = luxon.DateTime.local().hour;
 
 function addTimeBlock(hour){
+    var bgColor
+
+    if(hour<currentHour){
+        bgColor = "bg-secondary"
+    }else if(hour>currentHour){
+        bgColor = "bg-success"
+    }else{
+        bgColor = "bg-danger"
+    }
+
     var timeRow = $("<div class='row'>");
     var bufferLeft = $("<div class='col-md-1'>");
     var hourBlock = $("<div class='col-md-1 border-top border-bottom border-secondary border-right'>");
-    var textBlock = $("<input type='text' data-hour-id="+hour+" class='col-md-8 bg-success border-top border-bottom border-white event-text'>");
+    var textBlock = $("<input type='text' data-hour-id="+hour+" class='col-md-8 "+bgColor+" border-top border-bottom border-white event-text'>");
     var saveBlock = $("<div class='col-md-1 bg-primary border-primary rounded-right'><button data-button-hour="+hour+" class='btn btn-primary save-button'>Save</button></div>")
     var bufferRight = $("<div class='col-md-1'>");
 
-    hourBlock.text(hour);
+    if(hour>12){
+        hourBlock.text(hour-12+"PM")
+    }else{
+        hourBlock.text(hour+"AM")
+    }
+
     textBlock.attr("value","notes");
 
     timeRow.append(bufferLeft,hourBlock,textBlock,saveBlock,bufferRight);
