7783c414e0a31d426e4f72415360371110007c68
Todd Malone <toddomalone@gmail.com>
2021-01-18 20:06:20 -0600

    timeblock generation is now a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,20 @@
 </div> */}
 
 
-var timeRow = $("<div class='row'>");
-var bufferLeft = $("<div class='col-md-1'>");
-var hourBlock = $("<div class='col-md-1 border-top border-bottom border-secondary border-right'>");
-var textBlock = $("<div class='col-md-8 bg-success border-top border-bottom border-white'>");
-var saveBlock = $("<div class='col-md-1 bg-primary border-primary rounded-right'><button class='btn btn-primary'>Save</button></div>")
-var bufferRight = $("<div class='col-md-1'>");
 
-hourBlock.text("hour");
-textBlock.text("notes");
+function addTimeBlock(){
+    var timeRow = $("<div class='row'>");
+    var bufferLeft = $("<div class='col-md-1'>");
+    var hourBlock = $("<div class='col-md-1 border-top border-bottom border-secondary border-right'>");
+    var textBlock = $("<div class='col-md-8 bg-success border-top border-bottom border-white'>");
+    var saveBlock = $("<div class='col-md-1 bg-primary border-primary rounded-right'><button class='btn btn-primary'>Save</button></div>")
+    var bufferRight = $("<div class='col-md-1'>");
 
-timeRow.append(bufferLeft,hourBlock,textBlock,saveBlock,bufferRight);
+    hourBlock.text("hour");
+    textBlock.text("notes");
 
-$("#timeblock-container").append(timeRow);
+    timeRow.append(bufferLeft,hourBlock,textBlock,saveBlock,bufferRight);
+    $("#timeblock-container").append(timeRow);
+}
+
+addTimeBlock();
